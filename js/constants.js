97d39f25d7e0c0eee3405f3e86d8a59727d60751
Anna.Mast <95618481+AnnaMnesia@users.noreply.github.com>
2023-04-24 18:47:05 +0200

    initial commit

---
+++ js/constants.js
@@ -0,0 +1,22 @@
+export const BOOKS_API_URL = 'https://edwardtanguay.vercel.app/share/books.json';
+export const BOOKS_LIST_KEY = 'booksList';
+export const READ_LIST_KEY = 'readingList';
+
+// export * from './constants.js';
+
+
+// // module.js
+// export const foo = 'foo';
+// export const bar = 'bar';
+// export const baz = 'baz';
+
+// // export all named exports using the wildcard syntax under the name myModule
+// export * as myModule from './module.js'
+
+// // and when we import them in another js file we can access them like(`myModule.foo`, `myModule.bar`, etc)
+
+// // Can we use this instead ? I mean without the name myModule?
+// export * from './module.js';
+
+// // AND IF YES how i'll access them when i'll import them directly with their variable names? 
+
